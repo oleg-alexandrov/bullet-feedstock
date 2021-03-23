6faa9eec820e7b749ceedaf8194b05dddff89492
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-03-23 15:14:20 +0100

    try building 3.09

---
+++ recipe/bld_cpp.bat
@@ -0,0 +1,33 @@
+setlocal enableextensions
+
+mkdir build
+cd build
+if errorlevel 1 exit 1
+
+cmake .. ^
+  -GNinja ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+  -DBUILD_SHARED_LIBS=ON ^
+  -DINSTALL_LIBS=ON ^
+  -DINSTALL_EXTRA_LIBS=ON ^
+  -DBUILD_PYBULLET=ON ^
+  -DBUILD_PYBULLET_NUMPY=ON ^
+  -DBUILD_OPENGL3_DEMOS=ON ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DCMAKE_SKIP_RPATH=YES
+
+if errorlevel 1 exit 1
+
+ninja install
+
+if errorlevel 1 exit 1
+
+copy examples\ExampleBrowser\App_ExampleBrowser %PREFIX%\Library\bin\BulletExampleBrowser
+if errorlevel 1 exit 1
+
+mkdir %LIBRARY_PREFIX%\opt\bullet
+if errorlevel 1 exit 1
+xcopy /s data %LIBRARY_PREFIX%\opt\bullet
+if errorlevel 1 exit 1
